44683d6e87d62c84d9e402ccc5db24e97f954f16
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-13 14:38:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,10 +43,7 @@ def apply_coupons(cart, coupons)
         if cart[i][:count] >= coupons[j][:num] # if there is a coupon, compare the count in cart with coupon num. 
           num_applications = cart[i][:count] / coupons[i][:num]
           if num_applications > 1
-            puts cart
-            puts coupons
             num_disc_items = num_applications * coupons[j][:num]
-            puts num_disc_items
             count_after_coupons = cart[i][:count] - num_disc_items
             cart[i][:count] = count_after_coupons
             item_name = cart[i][:item]
